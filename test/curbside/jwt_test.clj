664246a39f51b41395a99f6d951e318dce732da6
Mike Sassak <msassak@gmail.com>
2017-04-13 13:06:26 -0700

    Make signing and encryption verification optional

--- test/curbside/jwt_test.clj
+++ test/curbside/jwt_test.clj
@@ -158,51 +158,75 @@
                   :encrypt-key rsa-jwk}))
 
         process (fn process
-                  ([claims]
-                   (process claims (constantly true)))
-                  ([claims verifier]
+                  ([jwt]
+                   (process jwt (constantly true)))
+                  ([jwt verifier]
                    (process-jwt
                      {:signing-alg :rs256
                       :encrypt-alg :rsa-oaep
                       :encrypt-enc :a256gcm
                       :keys [rsa-jwk]
-                      :jwt (nest claims)
+                      :jwt jwt
                       :verifier verifier})))]
 
   (testing "by default verifies exp and nbf, if they are present"
     (is (= {:iss "curbside.com" :aud #{"curbside.com"} :sub "jim"}
-           (process std-claims)))
+           (process (nest std-claims))))
 
     (is (thrown? Exception
                  (process
-                   (assoc std-claims
-                          :exp (t/minus (t/now) (t/days 7))))))
+                   (nest
+                     (assoc std-claims
+                            :exp (t/minus (t/now) (t/days 7)))))))
 
     (is (thrown? Exception
                  (process
-                   (assoc std-claims
-                          :nbf (t/plus (t/now) (t/days 7)))))))
+                   (nest
+                     (assoc std-claims
+                            :nbf (t/plus (t/now) (t/days 7))))))))
 
   (testing "allows custom claims verification"
     (is (= {:iss "curbside.com" :aud #{"curbside.com"} :sub "jim"}
-           (process std-claims
+           (process (nest std-claims)
                     (fn [claims-set]
                       (and (= (:iss claims-set) "curbside.com")
                            (contains? (:aud claims-set) "curbside.com"))))))
 
     (is (thrown? Exception
-           (process (assoc std-claims
-                           :exp (t/minus (t/now) (t/days 7)))
-                    (fn [claims-set]
-                      (and (= (:iss claims-set) "curbside.com")
-                           (contains? (:aud claims-set) "curbside.com"))))))
+                 (process (nest (assoc std-claims
+                                       :exp (t/minus (t/now) (t/days 7))))
+                          (fn [claims-set]
+                            (and (= (:iss claims-set) "curbside.com")
+                                 (contains? (:aud claims-set) "curbside.com"))))))
 
     (is (thrown? Exception
-                 (process std-claims
+                 (process (nest std-claims)
                           (fn [claims-set]
                             (and (= (:iss claims-set) "blurbside.com")
                                  (contains? (:aud claims-set) "bopis")))))))
 
+
+  (testing "accepts signed JWTs"
+    (is (= {:iss "curbside.com" :aud #{"curbside.com"} :sub "jim"}
+           (process-jwt
+             {:signing-alg :rs256
+              :keys [rsa-jwk]
+              :jwt (sign-jwt
+                     {:signing-alg :rs256
+                      :claims std-claims
+                      :signing-key rsa-jwk})}))))
+
+  (testing "accepts encrypted JWTs"
+    (is (= {:iss "curbside.com" :aud #{"curbside.com"} :sub "jim"}
+           (process-jwt
+             {:encrypt-alg :rsa-oaep
+              :encrypt-enc :a256gcm
+              :keys [rsa-jwk]
+              :jwt (encrypt-jwt
+                    {:encrypt-alg :rsa-oaep
+                     :encrypt-enc :a256gcm
+                     :claims std-claims
+                     :encrypt-key rsa-jwk})}))))
 
   (testing "rejects unsecured / plain JWTs")
   (testing "rejects downgraded crypto attacks")))
