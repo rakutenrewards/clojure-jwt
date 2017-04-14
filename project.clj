a90935aaae6f6d8afd2575f2014b1e446c372452
Connor Clark <connor@curbside.in>
2017-04-14 13:15:00 -0700

    add description and url, set up releases info

--- project.clj
+++ project.clj
@@ -1,13 +1,18 @@
 (defproject curbside-jwt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "JWT processing based on Nimbus JOSE + JWT"
+  :url "https://github.com/Curbside/curbside-jwt"
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [clj-time "0.13.0"]
                  [com.nimbusds/nimbus-jose-jwt "4.35"
                   :exclusions [net.minidev/json-smart]]
                  [net.minidev/json-smart "1.3.1"]
                  [cheshire "5.7.0"]
-                 [medley "0.8.4"]
-                 [org.clojure/tools.trace "0.7.9"]]
+                 [medley "0.8.4"]]
   :plugins [[lein-cljfmt "0.5.6"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
+                                  [org.clojure/tools.trace "0.7.9"]]}}
+  :deploy-repositories
+  [["releases"
+    {:url "https://curbside.jfrog.io/curbside/libs-release-local/"
+     :username :env/artifactory_user
+     :password :env/artifactory_pass}]])
