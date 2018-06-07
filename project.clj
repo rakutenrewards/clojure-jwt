9ebee50db163f04b37ba6f367c61c3aefbb2425c
Connor Clark <connor@curbside.in>
2018-06-07 08:49:19 -0700

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.curbside/curbside-jwt "0.1.7-SNAPSHOT"
+(defproject com.curbside/curbside-jwt "0.1.7"
   :description "JWT processing based on Nimbus JOSE + JWT"
   :url "https://github.com/Curbside/curbside-jwt"
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
